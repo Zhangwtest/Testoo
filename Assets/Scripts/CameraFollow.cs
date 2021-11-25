1972994e49aea2aeed8a93f60e1da68f979de0ff
Zhangwtest <2764901@qq.com>
2021-11-25 18:54:49 +0800

    2、修改障碍物生成的效果
    1、可以在新场景上刷新了，但是位置还有问题

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -18,7 +18,7 @@ public class CameraFollow : MonoBehaviour
             if (transform.position != player.position)
             {
                 //transform.position = player.position;
-                transform.position = new Vector3(player.position.x, player.position.y, player.position.z - 0.5f);
+                transform.position = new Vector3(player.position.x + 4.5f, player.position.y + 1f, player.position.z - 0.85f);
             }
         }
     }
--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -25,19 +25,19 @@ public class Ground : MonoBehaviour
         switch (x)
         {
             case 0:
-                Vector3 newPos_1 = new Vector3(Random.Range(-9,9), 2.8f, 0);
+                Vector3 newPos_1 = new Vector3(Random.Range(-9,9) + Index * 18f, 2.8f, 0);
                 Instantiate(obstacle, newPos_1, Quaternion.identity);
                 break;
             case 1:
-                Vector3 newPos_2 = new Vector3(Random.Range(-9, 9), 1.0f, 0);
+                Vector3 newPos_2 = new Vector3(Random.Range(-9, 9) + Index * 18f, 1.0f, 0);
                 Instantiate(obstacle, newPos_2, Quaternion.identity);
                 break;
             case 2:
-                Vector3 newPos_3 = new Vector3(Random.Range(-9, 9), -1.0f, 0);
+                Vector3 newPos_3 = new Vector3(Random.Range(-9, 9) + Index * 18f, -1.0f, 0);
                 Instantiate(obstacle, newPos_3, Quaternion.identity);
                 break;
             case 3:
-                Vector3 newPos_4 = new Vector3(Random.Range(-9, 9), -2.8f, 0);
+                Vector3 newPos_4 = new Vector3(Random.Range(-9, 9) + Index * 18f, -2.8f, 0);
                 Instantiate(obstacle, newPos_4, Quaternion.identity);
                 break;
         }
